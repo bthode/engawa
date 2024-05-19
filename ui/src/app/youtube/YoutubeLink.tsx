5bfe05977af31f8dffb7823042066256b405738a
Bryan Thode <bryan@tatq.com>
2024-05-19 06:34:46 -0500

    Add Youtube link fetcher component to placeholder homepage.
    Add material-ui and axios

---
+++ ui/src/app/youtube/YoutubeLink.tsx
@@ -0,0 +1,8 @@
+import dynamic from 'next/dynamic';
+
+const App = dynamic(
+  () => import('./YoutubeLinkFetcher'), // replace with the path to your component
+  { ssr: false } // This will disable server-side rendering (SSR)
+);
+
+export default App;
