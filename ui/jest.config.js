420fb10e57387a472312226f72df2caec601392b
Bryan Thode <bryan@tatq.com>
2024-05-20 07:15:45 -0500

    Use SWC compiler

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    '^.+\\.(t|j)sx?$': '@swc/jest',
   },
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': '<rootDir>/node_modules/jest-css-modules',
   },
 };
-
