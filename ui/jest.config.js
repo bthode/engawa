f60a8a0d940d7a415993676279ba99e804f721e7
Bryan Thode <bryan@tatq.com>
2024-06-05 05:15:30 -0500

    Set up aliases and some test dep's to allow for that.

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -1,9 +1,13 @@
 module.exports = {
+  preset: 'ts-jest',
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
   },
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': '<rootDir>/node_modules/jest-css-modules',
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@utilities/(.*)$': '<rootDir>/src/utilities/$1',
   },
 };
