1fbbdb0f3b705f4383bd7c807e396aaa253efdc8
Bryan Thode <bryan@tatq.com>
2024-06-21 05:25:17 -0500

    Don't hardcode plex values for local dev

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // env: {
+  //   PLEX_HOST: process.env.NEXT_PUBLIC_PLEX_HOST || '192.168.1.10',
+  // },
   async rewrites() {
     return [
       {
