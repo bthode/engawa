82c7947a3ea358c862ce2f984348a71a51f7a34a
Bryan Thode <bryan@tatq.com>
2024-09-23 04:54:37 -0500

    .

--- api/tests/test_scheduler.py
+++ api/tests/test_scheduler.py
@@ -6,9 +6,17 @@ from pytz import utc
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlmodel import SQLModel, select
 
-from app.models.subscription import Subscription, Video, VideoStatus
+from app.models.subscription import (
+    ComparisonOperator,
+    Filter,
+    FilterType,
+    Subscription,
+    Video,
+    VideoStatus,
+)
 from app.scheduler import (
     SUBSCRIPTION_UPDATE_INTERVAL,
+    get_pending_videos,
     get_subscriptions_to_update,
     sync_and_update_videos,
 )
@@ -21,7 +29,28 @@ from app.yt_downloader import (
 )
 
 
-async def mock_get_metadata(urls: list[str]):
+async def mock_success_video_data(
+    uploaded: datetime = datetime.now(),  # Default to current datetime
+    duration: int = 3600,  # Default to 1 hour
+    title: str = "Default Title",  # Default title
+    description: str = "Default Description"  # Default description
+) -> list[MetadataSuccess]:
+    return [MetadataSuccess(
+        url="http//example.com/video",
+        metadata=VideoMetadata(
+            id="test_video_id",
+            title=title,
+            uploader="Test Uploader",
+            upload_date=uploaded,
+            duration_in_seconds=duration,
+            description=description,
+            thumbnail_url="http://example.com/test_thumbnail.jpg",
+        ),
+    )
+    ]
+
+
+async def mock_success_metadata(urls: list[str]):
     return [
         MetadataSuccess(
             url=urls[0],
@@ -38,6 +67,22 @@ async def mock_get_metadata(urls: list[str]):
     ]
 
 
+async def mock_live_event_metadata(urls: list[str]):
+    return [
+        MetadataError(
+            url=urls[0],
+            error_type=MetadataErrorType.LIVE_EVENT_NOT_STARTED,
+            message="This live event will begin in a few moments",
+        )
+    ]
+
+
+async def mock_unavailable_metadata(urls: list[str]) -> list[MetadataResult]:
+    return [
+        MetadataError(url=urls[0], error_type=MetadataErrorType.VIDEO_UNAVAILABLE, message="This video is unavailable")
+    ]
+
+
 @pytest.fixture(scope="function")
 async def async_session_factory() -> AsyncGenerator[async_sessionmaker[AsyncSession], None]:
     engine = create_async_engine("sqlite+aiosqlite:///:memory:")
@@ -47,7 +92,6 @@ async def async_session_factory() -> AsyncGenerator[async_sessionmaker[AsyncSess
     async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
     yield async_session_maker
-
     await engine.dispose()
 
 
@@ -62,7 +106,7 @@ async def test_obtained_metadata(
 ):
     async_session = await anext(async_session_factory)
     async with async_session() as session:
-        monkeypatch.setattr("app.scheduler.get_metadata", mock_get_metadata)
+        monkeypatch.setattr("app.scheduler.get_metadata", mock_success_metadata)
         monkeypatch.setattr("app.scheduler.get_session", lambda: mock_get_session(session))  # type:ignore
 
         subscription = Subscription(
@@ -74,8 +118,7 @@ async def test_obtained_metadata(
             image="http://example.com/image.jpg",
         )
         video = Video(
-            id=1,
-            subscription_id=1,
+            subscription_id=subscription.id,
             status=VideoStatus.PENDING,
             link="http://example.com/video",
             author="Test Author",
@@ -100,22 +143,6 @@ async def test_obtained_metadata(
         assert updated_video.thumbnail_url == "http://example.com/test_thumbnail.jpg"
 
 
-async def mock_get_metadata_live_event(urls: list[str]):
-    return [
-        MetadataError(
-            url=urls[0],
-            error_type=MetadataErrorType.LIVE_EVENT_NOT_STARTED,
-            message="This live event will begin in a few moments",
-        )
-    ]
-
-
-async def mock_get_metadata_unavailable(urls: list[str]) -> list[MetadataResult]:
-    return [
-        MetadataError(url=urls[0], error_type=MetadataErrorType.VIDEO_UNAVAILABLE, message="This video is unavailable")
-    ]
-
-
 @pytest.mark.asyncio
 async def test_live_event_not_started(
     async_session_factory: AsyncGenerator[async_sessionmaker[AsyncSession], None],
@@ -123,7 +150,7 @@ async def test_live_event_not_started(
 ):
     async_session = await anext(async_session_factory)
     async with async_session() as session:
-        monkeypatch.setattr("app.scheduler.get_metadata", mock_get_metadata_live_event)
+        monkeypatch.setattr("app.scheduler.get_metadata", mock_live_event_metadata)
         monkeypatch.setattr("app.scheduler.get_session", lambda: mock_get_session(session))  # type:ignore
 
         subscription = Subscription(
@@ -135,8 +162,7 @@ async def test_live_event_not_started(
             image="http://example.com/image.jpg",
         )
         video = Video(
-            id=1,
-            subscription_id=1,
+            subscription_id=subscription.id,
             status=VideoStatus.PENDING,
             link="http://example.com/live",
             author="Test Author",
@@ -167,20 +193,21 @@ async def test_video_unavailable(
 ):
     async_session = await anext(async_session_factory)
     async with async_session() as session:
-        monkeypatch.setattr("app.scheduler.get_metadata", mock_get_metadata_unavailable)
+        monkeypatch.setattr("app.scheduler.get_metadata", mock_unavailable_metadata)
         monkeypatch.setattr("app.scheduler.get_session", lambda: mock_get_session(session))  # type: ignore
 
         subscription = Subscription(
-            id=1,
             title="Test Subscription",
             url="http://example.com",
             rss_feed_url="http://example.com/rss",
             description="Test Description",
             image="http://example.com/image.jpg",
         )
+        session.add(subscription)
+        await session.commit()
+        
         video = Video(
-            id=1,
-            subscription_id=1,
+            subscription_id=subscription.id,
             status=VideoStatus.PENDING,
             link="http://example.com/unavailable",
             author="Test Author",
@@ -190,7 +217,6 @@ async def test_video_unavailable(
             title="Test Video",
             video_id="test_video_id",
         )
-        session.add(subscription)
         session.add(video)
         await session.commit()
 
@@ -210,7 +236,7 @@ async def test_new_subscription_is_picked_up(
 ):
     async_session = await anext(async_session_factory)
     async with async_session() as session:
-        monkeypatch.setattr("app.scheduler.get_metadata", mock_get_metadata_unavailable)
+        monkeypatch.setattr("app.scheduler.get_metadata", mock_success_video_data)
         monkeypatch.setattr("app.scheduler.get_session", lambda: mock_get_session(session))  # type: ignore
 
         subscription = Subscription(
@@ -241,7 +267,7 @@ async def test_existing_subscription_is_picked_up(
 ):
     async_session = await anext(async_session_factory)
     async with async_session() as session:
-        monkeypatch.setattr("app.scheduler.get_metadata", mock_get_metadata_unavailable)
+        monkeypatch.setattr("app.scheduler.get_metadata", mock_unavailable_metadata)
         monkeypatch.setattr("app.scheduler.get_session", lambda: mock_get_session(session))  # type: ignore
 
         past_date: datetime = datetime.now(utc) - timedelta(minutes=SUBSCRIPTION_UPDATE_INTERVAL + 1)
@@ -275,7 +301,7 @@ async def test_get_subscriptions_to_update(
 ):
     async_session = await anext(async_session_factory)
     async with async_session() as session:
-        monkeypatch.setattr("app.scheduler.get_metadata", mock_get_metadata_unavailable)
+        monkeypatch.setattr("app.scheduler.get_metadata", mock_unavailable_metadata)
         monkeypatch.setattr("app.scheduler.get_session", lambda: mock_get_session(session))  # type: ignore
         new_subscription = Subscription(
             title="New Subscription",
@@ -314,3 +340,108 @@ async def test_get_subscriptions_to_update(
         assert any(sub.title == "New Subscription" for sub in subscriptions_to_update)
         assert any(sub.title == "Old Subscription" for sub in subscriptions_to_update)
         assert all(sub.title != "Recent Subscription" for sub in subscriptions_to_update)
+
+
+@pytest.mark.asyncio
+async def test_get_pending_videos(
+    async_session_factory: AsyncGenerator[async_sessionmaker[AsyncSession], None],
+    monkeypatch: pytest.MonkeyPatch,
+):
+    async_session = await anext(async_session_factory)
+    async with async_session() as session:
+        monkeypatch.setattr("app.scheduler.get_metadata", mock_success_video_data)
+        monkeypatch.setattr("app.scheduler.get_session", lambda: mock_get_session(session))  # type: ignore
+
+        subscription = Subscription(
+            title="New Subscription",
+            url="http://example.com/new",
+            rss_feed_url="http://example.com/new/rss",
+            description="Test Description",
+            image="http://example.com/image.jpg",
+        )
+        session.add(subscription)
+        await session.commit()
+
+        new_filter = Filter(
+            subscription_id=subscription.id,
+            filter_type=FilterType.DURATION,
+            comparison_operator=ComparisonOperator.GT,
+            threshold_seconds=1800,
+        )
+        session.add(new_filter)
+        await session.commit()
+
+        video = Video(
+            subscription_id=subscription.id,
+            status=VideoStatus.PENDING,
+            link="http://example.com/unavailable",
+            author="Test Author",
+            description="Test description",
+            published=datetime(2023, 1, 1),
+            thumbnail_url="http://example.com/thumbnail.jpg",
+            title="Test Video",
+            video_id="test_video_id",
+        )
+
+        session.add(video)
+        await session.commit()
+        
+        videos_to_update = await get_pending_videos(session)
+        assert isinstance(videos_to_update, list)
+        assert isinstance(videos_to_update[0], Video)
+        assert len(videos_to_update,) == 1
+
+@pytest.mark.asyncio
+async def test_video_filters(
+    async_session_factory: AsyncGenerator[async_sessionmaker[AsyncSession], None],
+    monkeypatch: pytest.MonkeyPatch,
+):
+    async_session = await anext(async_session_factory)
+    async with async_session() as session:
+        video_duration: int = 1800
+        monkeypatch.setattr("app.scheduler.get_metadata", mock_success_video_data(duration=video_duration - 100))
+        monkeypatch.setattr("app.scheduler.get_session", lambda: mock_get_session(session))  # type: ignore
+
+        subscription = Subscription(
+            title="New Subscription",
+            url="http://example.com/new",
+            rss_feed_url="http://example.com/new/rss",
+            description="Test Description",
+            image="http://example.com/image.jpg",
+        )
+        session.add(subscription)
+        await session.commit()
+
+        new_filter = Filter(
+            subscription_id=subscription.id,
+            filter_type=FilterType.DURATION,
+            comparison_operator=ComparisonOperator.GT,
+            threshold_seconds=video_duration,
+        )
+        session.add(new_filter)
+        await session.commit()
+
+        video = Video(
+            subscription_id=subscription.id,
+            status=VideoStatus.PENDING,
+            link="http://example.com/unavailable",
+            author="Test Author",
+            description="Test description",
+            published=datetime(2023, 1, 1),
+            thumbnail_url="http://example.com/thumbnail.jpg",
+            title="Test Video",
+            video_id="test_video_id",
+            duration=video_duration,
+        )
+
+        session.add(video)
+        await session.commit()
+        
+        videos_to_update = await get_pending_videos(session)
+        assert isinstance(videos_to_update, list)
+        assert isinstance(videos_to_update[0], Video)
+        assert len(videos_to_update,) == 1
+
+        #  TODO: Need to refactor the sync method to handle vidoes by subscription
+        #  Then we can call it for our above subscription
+        # and verify the video has been filtered out by the duration filter
