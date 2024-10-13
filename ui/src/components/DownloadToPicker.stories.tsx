6728b547fa5337b286bb32417998b4b3a5ed40fa
Bryan Thode <bryan@tatq.com>
2024-10-12 20:28:50 -0500

    Fix passing in props to FilterStep storybook

--- ui/src/components/DownloadToPicker.stories.tsx
+++ ui/src/components/DownloadToPicker.stories.tsx
@@ -1,12 +1,130 @@
+import { Directory, SaveToProps } from '@/types/plexTypes';
 import { Meta, StoryFn } from '@storybook/react';
+import { useState } from 'react';
 import DownloadToPicker from './DownloadToPicker';
 
 export default {
   title: 'Components/DownloadToPicker',
   component: DownloadToPicker,
+  argTypes: {
+    directories: { control: 'object' },
+    saveToProps: { control: 'object' },
+    setSaveToProps: { control: 'function' },
+  },
 } as Meta;
 
-const Template: StoryFn = (args) => <DownloadToPicker {...args} />;
+const directories: Directory[] = [
+  {
+    id: 2,
+    title: 'Movies',
+    uuid: '364f2ba8-254e-492d-a8d5-8658cfc90161',
+    key: 3,
+    locations: [
+      {
+        id: 3,
+        path: '/media/Media/Video/Movies',
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: 'Youtube',
+    uuid: '0c717b05-2deb-419c-a2d0-e68cceddea04',
+    key: 7,
+    locations: [
+      {
+        id: 5,
+        path: '/index/YouTube',
+      },
+      {
+        id: 6,
+        path: '/index/media',
+      },
+    ],
+  },
+  {
+    id: 7,
+    title: 'TV Shows',
+    uuid: '50b6e1e0-e274-41e4-ade8-1e71e95c9330',
+    key: 2,
+    locations: [
+      {
+        id: 8,
+        path: '/media/Media/Video/TV',
+      },
+    ],
+  },
+  {
+    id: 9,
+    title: 'Fitness',
+    uuid: 'e32e9e32-b24c-4867-b113-95fc3914e37e',
+    key: 16,
+    locations: [
+      {
+        id: 10,
+        path: '/index/YouTube/reference',
+      },
+    ],
+  },
+  {
+    id: 10,
+    title: 'Misc',
+    uuid: '2c507cdc-36d7-43cc-beee-efbee3644bbd',
+    key: 1,
+    locations: [
+      {
+        id: 11,
+        path: '/media/Media/Video/Video',
+      },
+    ],
+  },
+  {
+    id: 11,
+    title: 'Personal',
+    uuid: '3df269f4-004e-4c00-b522-4acb6c8dfe54',
+    key: 14,
+    locations: [
+      {
+        id: 12,
+        path: '/media/Media/Video/Personal',
+      },
+    ],
+  },
+];
+
+const Template: StoryFn<{
+  directories: Directory[];
+  saveToProps: SaveToProps;
+  setSaveToProps: React.Dispatch<React.SetStateAction<SaveToProps>>;
+}> = (args) => {
+  const [saveToProps, setSaveToProps] = useState<SaveToProps>(args.saveToProps);
+
+  return <DownloadToPicker {...args} saveToProps={saveToProps} setSaveToProps={setSaveToProps} />;
+};
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  directories,
+  saveToProps: {
+    directoryId: -1,
+    locationId: -1,
+  },
+};
+
+export const WithSelectedDirectory = Template.bind({});
+WithSelectedDirectory.args = {
+  directories,
+  saveToProps: {
+    directoryId: 7,
+    locationId: -1,
+  },
+};
+
+export const WithSelectedLocation = Template.bind({});
+WithSelectedLocation.args = {
+  directories,
+  saveToProps: {
+    directoryId: 2,
+    locationId: 8,
+  },
+};
