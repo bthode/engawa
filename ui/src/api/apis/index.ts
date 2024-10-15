fb1b61acd31563a244c04d5bad9a4606752d5e41
Bryan Thode <bryan@tatq.com>
2024-10-15 12:42:44 -0500

    Add generated Engawa api client

---
+++ ui/src/api/apis/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './PlexApi';
+export * from './SettingsApi';
+export * from './SubscriptionApi';
+export * from './YoutubeApi';
---
+++ ui/src/api/models/index.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ChannelInfo';
+export * from './ComparisonOperator';
+export * from './DirectoryPublic';
+export * from './ErrorState';
+export * from './Filter';
+export * from './FilterType';
+export * from './HTTPValidationError';
+export * from './LocationPublic';
+export * from './PlexLibraryDestination';
+export * from './PlexPublicWithDirectories';
+export * from './PlexServerCreate';
+export * from './RequirementStatus';
+export * from './RequirementsCheckResult';
+export * from './RetentionPolicyModel';
+export * from './RetentionType';
+export * from './Subscription';
+export * from './SubscriptionCreate';
+export * from './SubscriptionCreateV2';
+export * from './SubscriptionDirectoryError';
+export * from './SubscriptionType';
+export * from './ValidationError';
+export * from './ValidationErrorLocInner';
+export * from './Video';
+export * from './VideoStatus';
